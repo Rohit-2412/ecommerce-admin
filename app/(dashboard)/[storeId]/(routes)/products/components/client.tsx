faacf715cd901b1b0c7cfbc56fa2698923124862
Rohit <rk212300@outlook.com>
2023-11-08 14:44:57 +0530

    products done

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { ProductColumn, columns } from "./columns";
+import { useParams, useRouter } from "next/navigation";
+
+import { ApiList } from "@/components/ui/api-list";
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { Heading } from "@/components/ui/heading";
+import { PlusIcon } from "lucide-react";
+import React from "react";
+import { Separator } from "@/components/ui/separator";
+
+interface ProductClientProps {
+    data: ProductColumn[];
+}
+
+export const ProductClient: React.FC<ProductClientProps> = ({ data }) => {
+    const router = useRouter();
+    const params = useParams();
+
+    return (
+        <>
+            <div className="flex items-center justify-between">
+                <Heading
+                    title={`Products (${data?.length})`}
+                    description="Manage products for your store"
+                />
+                <Button
+                    onClick={() =>
+                        router.push(`/${params.storeId}/products/new`)
+                    }
+                >
+                    <PlusIcon className="mr-2 h-4 w-4" /> Add New
+                </Button>
+            </div>
+            <Separator />
+            <DataTable columns={columns} data={data} searchKey="name" />
+            <Heading title="API" description="API calls for Products" />
+            <Separator />
+            <ApiList entityIdName="productId" entityName="products" />
+        </>
+    );
+};
